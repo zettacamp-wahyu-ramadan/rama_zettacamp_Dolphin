6d74328816c2e461efb87e5bc7c98ae24e20bd3a
zettacamp-wahyu-ramadan <wahyu.ramadan@zettacamp.pro>
2023-08-30 15:41:58 +0700

    initialization js day 7

---
+++ listSong.js
@@ -0,0 +1,387 @@
+// List song
+const listSong = [
+  {
+    id: 1,
+    title: 'Shape of You',
+    artist: 'Ed Sheeran',
+    genre: 'Pop',
+    duration: '3.54',
+    isPlay: false,
+  },
+  {
+    id: 2,
+    title: 'Bohemian Rhapsody',
+    artist: 'Queen',
+    genre: 'Rock',
+    duration: '5.55',
+    isPlay: false,
+  },
+  {
+    id: 3,
+    title: 'Rolling in the Deep',
+    artist: 'Adele',
+    genre: 'Pop',
+    duration: '3.48',
+    isPlay: false,
+  },
+  {
+    id: 4,
+    title: 'Billie Jean',
+    artist: 'Michael Jackson',
+    genre: 'Pop',
+    duration: '4.54',
+    isPlay: false,
+  },
+  {
+    id: 5,
+    title: 'Hotel California',
+    artist: 'Eagles',
+    genre: 'Rock',
+    duration: '6.30',
+    isPlay: false,
+  },
+  {
+    id: 6,
+    title: 'Uptown Funk',
+    artist: 'Mark Ronson ft. Bruno Mars',
+    genre: 'Funk',
+    duration: '4.30',
+    isPlay: false,
+  },
+  {
+    id: 7,
+    title: 'Wonderwall',
+    artist: 'Oasis',
+    genre: 'Rock',
+    duration: '4.18',
+    isPlay: false,
+  },
+  {
+    id: 8,
+    title: 'Shape of My Heart',
+    artist: 'Sting',
+    genre: 'Pop',
+    duration: '4.38',
+    isPlay: false,
+  },
+  {
+    id: 9,
+    title: 'Smooth',
+    artist: 'Santana ft. Rob Thomas',
+    genre: 'Rock',
+    duration: '4.00',
+    isPlay: false,
+  },
+  {
+    id: 10,
+    title: 'Hello',
+    artist: 'Adele',
+    genre: 'Pop',
+    duration: '4.55',
+    isPlay: false,
+  },
+  {
+    id: 11,
+    title: "Livin' on a Prayer",
+    artist: 'Bon Jovi',
+    genre: 'Rock',
+    duration: '4.10',
+    isPlay: false,
+  },
+  {
+    id: 12,
+    title: 'Dancing Queen',
+    artist: 'ABBA',
+    genre: 'Pop',
+    duration: '3.52',
+    isPlay: false,
+  },
+  {
+    id: 13,
+    title: "Sweet Child o' Mine",
+    artist: "Guns N' Roses",
+    genre: 'Rock',
+    duration: '5.55',
+    isPlay: false,
+  },
+  {
+    id: 14,
+    title: "Stayin' Alive",
+    artist: 'Bee Gees',
+    genre: 'Disco',
+    duration: '4.45',
+    isPlay: false,
+  },
+  {
+    id: 15,
+    title: 'Thinking Out Loud',
+    artist: 'Ed Sheeran',
+    genre: 'Pop',
+    duration: '4.41',
+    isPlay: false,
+  },
+  {
+    id: 16,
+    title: 'I Want to Hold Your Hand',
+    artist: 'The Beatles',
+    genre: 'Rock',
+    duration: '2.24',
+    isPlay: false,
+  },
+  {
+    id: 17,
+    title: 'All of Me',
+    artist: 'John Legend',
+    genre: 'Pop',
+    duration: '4.29',
+    isPlay: false,
+  },
+  {
+    id: 18,
+    title: 'Somebody to Love',
+    artist: 'Queen',
+    genre: 'Rock',
+    duration: '4.56',
+    isPlay: false,
+  },
+  {
+    id: 19,
+    title: 'Stay',
+    artist: 'Rihanna ft. Mikky Ekko',
+    genre: 'Pop',
+    duration: '4.00',
+    isPlay: false,
+  },
+  {
+    id: 20,
+    title: 'Highway to Hell',
+    artist: 'AC/DC',
+    genre: 'Rock',
+    duration: '3.28',
+    isPlay: false,
+  },
+  {
+    id: 21,
+    title: 'Radioactive',
+    artist: 'Imagine Dragons',
+    genre: 'Pop',
+    duration: '3.07',
+    isPlay: false,
+  },
+  {
+    id: 22,
+    title: "Don't Stop Believin'",
+    artist: 'Journey',
+    genre: 'Rock',
+    duration: '4.09',
+    isPlay: false,
+  },
+  {
+    id: 23,
+    title: 'Chandelier',
+    artist: 'Sia',
+    genre: 'Pop',
+    duration: '3.36',
+    isPlay: false,
+  },
+  {
+    id: 24,
+    title: 'Yesterday',
+    artist: 'The Beatles',
+    genre: 'Rock',
+    duration: '2.05',
+    isPlay: false,
+  },
+  {
+    id: 25,
+    title: "Can't Help Falling in Love",
+    artist: 'Elvis Presley',
+    genre: 'Rock',
+    duration: '3.00',
+    isPlay: false,
+  },
+  {
+    id: 26,
+    title: 'Happy',
+    artist: 'Pharrell Williams',
+    genre: 'Pop',
+    duration: '3.53',
+    isPlay: false,
+  },
+  {
+    id: 27,
+    title: 'Piano Man',
+    artist: 'Billy Joel',
+    genre: 'Pop',
+    duration: '5.37',
+    isPlay: false,
+  },
+  {
+    id: 28,
+    title: 'Stairway to Heaven',
+    artist: 'Led Zeppelin',
+    genre: 'Rock',
+    duration: '8.02',
+    isPlay: false,
+  },
+  {
+    id: 29,
+    title: 'Someone Like You',
+    artist: 'Adele',
+    genre: 'Pop',
+    duration: '4.45',
+    isPlay: false,
+  },
+  {
+    id: 30,
+    title: 'Hallelujah',
+    artist: 'Leonard Cohen',
+    genre: 'Folk',
+    duration: '4.39',
+    isPlay: false,
+  },
+  {
+    id: 31,
+    title: 'Radio Ga Ga',
+    artist: 'Queen',
+    genre: 'Rock',
+    duration: '5.43',
+    isPlay: false,
+  },
+  {
+    id: 32,
+    title: 'Hey Jude',
+    artist: 'The Beatles',
+    genre: 'Rock',
+    duration: '7.11',
+    isPlay: false,
+  },
+  {
+    id: 33,
+    title: 'Thriller',
+    artist: 'Michael Jackson',
+    genre: 'Pop',
+    duration: '5.57',
+    isPlay: false,
+  },
+  {
+    id: 34,
+    title: 'Back in Black',
+    artist: 'AC/DC',
+    genre: 'Rock',
+    duration: '4.15',
+    isPlay: false,
+  },
+];
+
+// const listSong = [
+//   {
+//     id: 1,
+//     title: 'Shape of You',
+//     artist: 'Ed Sheeran',
+//     genre: 'Pop',
+//     duration: '29.55',
+//     isPlay: false,
+//   },
+//   {
+//     id: 2,
+//     title: 'Bohemian Rhapsody',
+//     artist: 'Queen',
+//     genre: 'Rock',
+//     duration: '30.05',
+//     isPlay: false,
+//   },
+// ];
+
+// Grouping songs by artist
+const groupByArtist = (artistParam) => {
+  // Set param to lower case
+  artistParam.toLowerCase();
+  // Find list song by artist
+  // Set artist from callback filtering to lowercase and find if it contain with artist from param
+  const filteringByArtist = listSong.filter((valueSong) =>
+    valueSong.artist.toLowerCase().includes(artistParam)
+  );
+
+  return filteringByArtist;
+};
+
+// Grouping songs by genre
+const groupByGenre = (genreParam) => {
+  // Set param to lowercase
+  genreParam.toLowerCase();
+  // Find list song by genre
+  // Set genre from callback filtering to lowecase and find if it contain with genre from param
+  const filteringByGenre = listSong.filter((valueSong) =>
+    valueSong.genre.toLowerCase().includes(genreParam)
+  );
+
+  return filteringByGenre;
+};
+
+// Calculate duration max 1 hour
+// const groupSongByMaxDuration1Hour = () => {
+//   let initialDurationMinute = 0;
+//   let initialDurationSecond = 0;
+//   let remainingReduction = 0;
+//   // Get the duration all list song
+//   const filterSong = listSong.filter((valueDuration, index) => {
+//     const duration = valueDuration.duration;
+//     const minuteOfSong = parseInt(duration.split('.')[0]);
+//     const secondOfSong = parseInt(duration.split('.')[1]);
+//     initialDurationSecond += secondOfSong;
+//     initialDurationMinute += minuteOfSong;
+//     // console.log(`MINUTE : ${initialDurationMinute}, SECOND : ${initialDurationSecond}`)
+//     if (initialDurationSecond >= 60) {
+//       initialDurationMinute += 1;
+//       // console.log(`index ${index} : originalSecond ${initialDurationSecond}: remain second ${remainingReduction} : minutes ${initialDurationMinute}`);
+//       remainingReduction = initialDurationSecond - 60;
+//       // console.log(`index ${index} : remain second ${remainingReduction} : minutes ${initialDurationMinute}`);
+//       initialDurationSecond = remainingReduction;
+
+//       if (initialDurationMinute > 60 && initialDurationSecond > 0) {
+//         // console.log('YES MINUTES', initialDurationMinute);
+//         // console.log(minuteOfSong);
+//         // console.log(initialDurationMinute)
+//         return false;
+//       }
+
+//       return valueDuration;
+//     } else {
+//       // console.log(initialDurationMinute)
+//       if (initialDurationMinute === 60 && initialDurationSecond > 0)
+//         return false;
+//       return valueDuration;
+//     }
+//   });
+//   return filterSong;
+// };
+
+const groupSongByMaxDuration1Hour = (songsParam) => {
+  let initialDurationSecond = 0;
+  const maxLength = 60 * 60;
+  // Get the duration all list song
+  const filterSong = songsParam.filter((valueDuration, index) => {
+    const duration = valueDuration.duration;
+    const minuteOfSong = parseInt(duration.split('.')[0]);
+    const secondOfSong = parseInt(duration.split('.')[1]);
+    const convertMinutesToSecond = minuteOfSong * 60;
+    const totalDurationSecondPerSong = convertMinutesToSecond + secondOfSong;
+    initialDurationSecond += totalDurationSecondPerSong;
+
+    if (initialDurationSecond <= maxLength) return valueDuration;
+
+    return false;
+  });
+  return filterSong;
+};
+
+console.log('Search by artist name', groupByArtist('el'));
+console.log('');
+console.log('------------------------------------------');
+console.log('');
+console.log('Search by genre', groupByGenre('roc'));
+console.log('');
+console.log('------------------------------------------');
+console.log('');
+console.log('List song with max 1 hour is', groupSongByMaxDuration1Hour(listSong));
