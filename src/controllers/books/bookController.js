91cd68f7dc1b60edc7d1a02e767b4db12a85bf03
zettaamp-wahyu-ramadan <wahyu.ramadan@zettacamp.pro>
2023-09-25 16:28:47 +0700

    done task mongodb day 7

--- src/controllers/books/bookController.js
+++ src/controllers/books/bookController.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const httpStatus = require('http-status');
 const moment = require('moment');
 const {
@@ -9,6 +10,8 @@ const {
   distinctBookService,
   aggregationBookService,
 } = require('../../services/books/bookService');
+
+const { NODE_PORT, NODE_SERVER } = process.env;
 
 const createBookController = async (req, res) => {
   try {
@@ -35,9 +38,216 @@ const createBookController = async (req, res) => {
 
 const findAllBookController = async (req, res) => {
   try {
-    const books = await findAllBookService();
+    let { page, limit } = req.query;
 
-    res.sendWrapped('List of books', httpStatus.OK, books);
+    if (!page) {
+      page = 1;
+    } else if (page < 1) {
+      page = 1;
+    } else {
+      page = Number(page);
+    }
+
+    if (!limit) {
+      limit = 5;
+    } else {
+      limit = Number(limit);
+    }
+
+    const calculateSkip = (page - 1) * limit;
+
+    // const countDataPipeline = [
+    //   {
+    //     $group: {
+    //       _id: null,
+    //       count: { $sum: 1 },
+    //     },
+    //   },
+    // ];
+
+    // const limitAndSkipPipeline = [
+    //   {
+    //     $skip: calculateSkip,
+    //   },
+    //   {
+    //     $limit: limit,
+    //   },
+    // ];
+
+    const limitAndSkipPipeline = [
+      {
+        $facet: {
+          books: [
+            {
+              $sort: {
+                updated_at: -1
+              }
+            },
+            {
+              $skip: calculateSkip,
+            },
+            {
+              $limit: limit,
+            },
+          ],
+          totalPage: [
+            {
+              $group: {
+                _id: null,
+                totalData: { $sum: 1 },
+              },
+            },
+          ],
+        },
+      },
+      {
+        $addFields: {
+          totalData: { $arrayElemAt: ['$totalPage.totalData', 0] },
+        },
+      },
+      {
+        $addFields: {
+          calculateTotalPage: {
+            $round: [
+              {
+                $divide: ['$totalData', limit],
+              },
+            ],
+          },
+        },
+      },
+      {
+        $addFields: {
+          calculateModPage: {
+            $cond: {
+              if: {
+                $and: [
+                  { $eq: ['$calculateTotalPage', 1] },
+                  { $ne: ['$totalData', limit] },
+                ],
+              },
+              then: 1,
+              else: { $mod: ['$totalData', '$calculateTotalPage'] },
+            },
+          },
+        },
+      },
+      {
+        $addFields: {
+          totalPage: {
+            $cond: {
+              if: { $gte: ['$calculateModPage', 1] },
+              then: { $add: ['$calculateTotalPage', 1] },
+              else: '$calculateTotalPage',
+            },
+          },
+        },
+      },
+      {
+        $addFields: {
+          limit: limit,
+        },
+      },
+      {
+        $addFields: {
+          page: page,
+        },
+      },
+      {
+        $addFields: {
+          isPrevPage: {
+            $cond: {
+              if: { $lte: ['$page', 1] },
+              then: false,
+              else: true,
+            },
+          },
+        },
+      },
+      {
+        $addFields: {
+          isNextPage: {
+            $cond: {
+              if: { $eq: ['$page', '$totalPage'] },
+              then: false,
+              else: true,
+            },
+          },
+        },
+      },
+      {
+        $addFields: {
+          prevPage: {
+            $cond: {
+              if: { $lte: ['$page', 1] },
+              then: null,
+              else: `${NODE_SERVER}:${NODE_PORT}/v1/book?limit=${limit}&page=${
+                page - 1
+              }`,
+            },
+          },
+        },
+      },
+      {
+        $addFields: {
+          nextPage: {
+            $cond: {
+              if: { $eq: ['$page', '$totalPage'] },
+              then: null,
+              else: `${NODE_SERVER}:${NODE_PORT}/v1/book?limit=${limit}&page=${
+                page + 1
+              }`,
+            },
+          },
+        },
+      },
+      {
+        $project: {
+          books: '$books',
+          totalData: '$totalData',
+          totalPage: '$totalPage',
+          limit: '$limit',
+          currentPage: '$page',
+          isPrevPage: '$isPrevPage',
+          isNextPage: '$isNextPage',
+          prevPage: '$prevPage',
+          nextPage: '$nextPage',
+        },
+      },
+    ];
+
+    // const countData = await aggregationBookService(countDataPipeline);
+
+    const books = await aggregationBookService(limitAndSkipPipeline);
+
+    // const calculateTotalPage =
+    //   countData[0].count % Math.round(countData[0].count / limit) !== 0
+    //     ? Math.round(countData[0].count / limit) + 1
+    //     : Math.round(countData[0].count / limit);
+
+    // const result = {
+    //   books,
+    //   page,
+    //   limit,
+    //   totalPage: calculateTotalPage,
+    //   // totalPage: 6%3,
+    //   isPrevPage: page <= 1 ? false : true,
+    //   isNextPage: page == calculateTotalPage ? false : true,
+    //   prevPage:
+    //     page <= 1
+    //       ? ''
+    //       : `${NODE_SERVER}:${NODE_PORT}/v1/book?limit=${limit}&page=${
+    //           page - 1
+    //         }`,
+    //   nextPage:
+    //     page == calculateTotalPage
+    //       ? ''
+    //       : `${NODE_SERVER}:${NODE_PORT}/v1/book?limit=${limit}&page=${
+    //           page + 1
+    //         }`,
+    // };
+
+    res.sendWrapped('List of books', httpStatus.OK, ...books);
   } catch (error) {
     console.error(`Error catch controller: ${error}`);
     throw new Error(error);
